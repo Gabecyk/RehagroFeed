3d41348d88c4b2fccb422ec57bb35168bbdfa183
FilipefromtheSilva <filipedasilvaoliveira2@gmail.com>
2025-08-05 16:44:36 -0300

    Adicionado SideBarProfile

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,14 @@
 import './index.css'
-
+import SidebarProfile from './components/SideBarProfile/SideBarProfile.jsx';
 import { Header } from './components/Header/Header.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <div style={{ padding: '20px', background: '#121214', height: '100vh' }}>
+      <SidebarProfile />
+    </div>
     </>
   )
 }
